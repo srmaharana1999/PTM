c256560f4911f615324b97c8c1a3398d22e4b40c
Soumya Ranjan Maharana <s.r.m.20.3.2018@gmail.com>
2026-02-10 19:06:24 +0530

    refactor: restructure the home page with tasks and filter feature

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -1,10 +1,13 @@
-
-
+import Tasks from "@/components/Tasks";
 const Home = () => {
-
   return (
-    <div className="w-full h-screen flex items-center justify-center">
-        This is Home Page.
+    <div className="max-w-7xl w-11/12 mx-auto mt-30">
+      <h1 className="text-3xl font-bold leading-relaxed">
+        Welcome,{" "}
+        <strong className="capitalize max-sm:block">John Doe 👋</strong>
+      </h1>
+
+      <Tasks />
     </div>
   );
 };
