1955e074ed040bd891d3e2044a1023026fa7a164
Soumya Ranjan Maharana <s.r.m.20.3.2018@gmail.com>
2026-03-17 23:44:54 +0530

    feat: wrap application in QueryClientProvider for React Query integration

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -7,15 +7,21 @@ import { Provider } from "react-redux";
 import { store } from "./app/store.ts";
 import Header from "./pages/Header.tsx";
 import { Toaster } from "react-hot-toast";
+import { QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import { queryClient } from "./lib/queryClient.ts";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <Provider store={store}>
-      <BrowserRouter>
-        <Header />
-        <Toaster position="top-center" />
-        <App />
-      </BrowserRouter>
+      <QueryClientProvider client={queryClient}>
+        <BrowserRouter>
+          <Header />
+          <Toaster position="top-center" />
+          <App />
+        </BrowserRouter>
+        {import.meta.env.DEV && <ReactQueryDevtools initialIsOpen={false} />}
+      </QueryClientProvider>
     </Provider>
   </StrictMode>,
 );
