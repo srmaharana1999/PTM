81db8f14a9107b33f1f7b7a55f3c451d31de9682
Soumya Ranjan Maharana <s.r.m.20.3.2018@gmail.com>
2026-03-17 23:44:44 +0530

    feat: refactor App component routes and improve task loading with Suspense

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,31 +2,42 @@ import { Route, Routes } from "react-router-dom";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
 import Dashboard from "./pages/Dashboard";
-import ProtectedRoute from "./routes/ProtectedRoute";
-import { useEffect } from "react";
+import { useEffect, Suspense } from "react";
 import { initAuth } from "./features/auth/authThunks";
 import { useAppDispatch } from "./app/hooks";
 import Home from "./pages/Home";
+import User from "./components/Dashboard/User";
+import CreateTask from "./components/Dashboard/CreateTask";
+import OverView from "./components/Dashboard/OverView";
+import TaskList from "./components/Dashboard/TaskList";
+
+import HookTester from "./components/HookTester";
 
 function App() {
   const dispatch = useAppDispatch();
-  useEffect(()=>{
-    dispatch(initAuth())
-  },[dispatch])
+  useEffect(() => {
+    dispatch(initAuth());
+  }, [dispatch]);
   return (
     <Routes>
-      <Route path="/" element={<Home/>}/>
+      <Route path="/" element={<Home />} />
       <Route path="/login" element={<Login />} />
       <Route path="/register" element={<Register />} />
+      <Route path="/testing" element={<HookTester />} />
 
-      <Route
-        path="/dashboard"
-        element={
-          <ProtectedRoute>
-            <Dashboard />
-          </ProtectedRoute>
-        }
-      />
+      <Route path="/dashboard" element={<Dashboard />}>
+        <Route index element={<OverView />} />
+        <Route path="profile" element={<User />} />
+        <Route
+          path="task_list"
+          element={
+            <Suspense fallback={<div className="container mx-auto py-10">Loading tasks...</div>}>
+              <TaskList />
+            </Suspense>
+          }
+        />
+        <Route path="create_task" element={<CreateTask />} />
+      </Route>
     </Routes>
   );
 }
