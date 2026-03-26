f420847c9b22bae85152e026f7c8f975c1ef6c42
Soumya Ranjan Maharana <s.r.m.20.3.2018@gmail.com>
2026-03-27 04:49:02 +0530

    feat: implement one bgcolor to header and hero section

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -2,9 +2,11 @@ import { Header, Hero } from "@/components/Home";
 
 const HomePage = () => {
   return (
-    <div className="bg-linear-to-b from-fuchsia-100 via-fuchsia-200 to-violet-300 ">
-      <Header />
-      <Hero />
+    <div>
+      <div className="bg-linear-to-b from-fuchsia-100 via-fuchsia-200 to-violet-300 ">
+        <Header />
+        <Hero />
+      </div>
     </div>
   );
 };
