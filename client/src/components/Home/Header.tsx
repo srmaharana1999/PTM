a8d9c48e4998edb087ea22bbbe4187eabe3a1fe4
Soumya Ranjan Maharana <s.r.m.20.3.2018@gmail.com>
2026-03-27 04:43:18 +0530

    feat: refactor the header & implement scrolling effect

--- client/src/components/Home/Header.tsx
+++ client/src/components/Home/Header.tsx
@@ -1,6 +1,6 @@
 import { cn } from "@/lib/utils";
 import { ArrowRight, MenuIcon, X } from "lucide-react";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { NavLink } from "react-router-dom";
 
 const navItems = [
@@ -21,17 +21,42 @@ const navItems = [
   },
 ];
 
-export const Header = () => {
+const Header = () => {
   const [open, setOpen] = useState(true);
+  const [scrolled, setScrolled] = useState(false);
+
+  useEffect(() => {
+    const handleScrolled = () => {
+      if (window.scrollY > 0) {
+        setScrolled(true);
+      } else {
+        setScrolled(false);
+      }
+    };
+    window.addEventListener("scroll", handleScrolled);
+
+    return () => window.removeEventListener("scroll", handleScrolled);
+  }, []);
+  console.log(scrolled);
   return (
-    <nav className="max-w-7xl fixed mx-auto inset-0 top-0 z-30">
-      <div className="h-20 px-6 border border-neutral-200 flex items-center justify-between">
-        <div className="bg-radial border-2 border-neutral-white from-violet-600 to-fuchsia-700 drop-shadow-sm drop-shadow-blue-400 w-fit h-14 aspect-square rounded-3xl flex items-center justify-center">
-          <p className="font-bold text-transparent tracking-wide bg-clip-text bg-linear-to-br text-lg from-violet-100 via-fuchsia-100 to-violet-200">
+    <nav
+      className={cn(
+        "sticky mx-auto inset-0 top-0 z-30",
+        scrolled ? "w-full bg-white shadow-md" : "max-w-7xl",
+      )}
+    >
+      <div
+        // className="h-20 max-w-7xl mx-auto px-6 flex items-center justify-between"
+        className={cn(
+          "h-16 max-w-7xl mx-auto px-6 flex items-center justify-between",
+        )}
+      >
+        <div className="bg-radial border-2 border-neutral-white from-violet-600 to-fuchsia-700 drop-shadow-sm drop-shadow-blue-400 w-fit h-12 aspect-square rounded-2xl flex items-center justify-center">
+          <p className="font-bold text-transparent tracking-wide bg-clip-text bg-linear-to-br from-violet-100 via-fuchsia-100 to-violet-200">
             PTM
           </p>
         </div>
-        <div className="flex items-center gap-10 max-md:hidden">
+        <div className="flex text-sm items-center gap-10 max-md:hidden">
           {navItems.map((item) => (
             <NavLink
               key={item.label}
@@ -49,6 +74,19 @@ export const Header = () => {
               {item.label}
             </NavLink>
           ))}
+          <NavLink
+            to="login"
+            className="capitalize font-medium text-neutral-500 hover:text-neutral-900"
+          >
+            Login
+          </NavLink>
+          <NavLink
+            to="/register"
+            className="bg-chart-1 drop-shadow-sm px-4 py-1.5 flex items-center gap-2 rounded-full"
+          >
+            <p>Get Started</p>
+            <ArrowRight />{" "}
+          </NavLink>
         </div>
 
         <button className="md:hidden" onClick={() => setOpen((pre) => !pre)}>
@@ -58,23 +96,10 @@ export const Header = () => {
             <X className="text-neutral-900" />
           )}
         </button>
-
-        <div className="flex items-center gap-4 max-md:hidden">
-          <NavLink
-            to="login"
-            className="text-neutral-800 hover:underline hover:underline-offset-8 under"
-          >
-            Login
-          </NavLink>
-          <div className="bg-chart-1 drop-shadow-sm px-4 py-2 flex items-center rounded-full">
-            <p>Get Started</p>
-            <ArrowRight />{" "}
-          </div>
-        </div>
       </div>
       <div
         className={cn(
-          "flex flex-col md:hidden gap-6 py-6 items-center bg-linear-to-b from-violet-200 to-fuchsia-200 border-b border-t border-white/60 transition-transform origin-top ease-in-out duration-500",
+          "absolute inset-x-0 flex flex-col md:hidden gap-6 py-6 items-center bg-linear-to-b from-violet-200 to-fuchsia-200 border-b border-t border-white/60 transition-transform origin-top ease-in-out duration-500",
           open ? "scale-y-0" : "scale-y-full",
         )}
       >
@@ -110,3 +135,5 @@ export const Header = () => {
     </nav>
   );
 };
+
+export default Header;
