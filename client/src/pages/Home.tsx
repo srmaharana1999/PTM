2a12d383b4adc481f9ac803c80bc60ea18b75a3f
Soumya Ranjan Maharana <s.r.m.20.3.2018@gmail.com>
2026-03-27 07:37:29 +0530

    feat: add home page in approuter and add all home sections

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -1,13 +1,13 @@
-import { Header, Hero } from "@/components/Home";
+import { CTASection, FeatureHighlight, Header, Hero } from "@/components/Home";
 
 const HomePage = () => {
   return (
-    <div>
-      <div className="bg-linear-to-b from-fuchsia-100 via-fuchsia-200 to-violet-300 ">
-        <Header />
-        <Hero />
-      </div>
-    </div>
+    <>
+      <Header />
+      <Hero />
+      <FeatureHighlight />
+      <CTASection />
+    </>
   );
 };
 
